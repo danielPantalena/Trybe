d8769408bf721a0f0dd2abdd4405d0627d6e017c
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-03-26 18:27:14 -0300

    exercise 1

---
+++ exercises/4_1/exercise_1.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 10;
+
+let result = a + b;
+console.log("a soma é " + result);
+
+result = a * b;
+console.log("A multiplicação é " + result);
+
+result = a / b;
+console.log("A divisão é " + result);
+
+result = a % b;
+console.log("O resto da divisão é " + result);

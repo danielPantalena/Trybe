b0feaaa9ccb7c05e22d7cf6be921b099dc809cb8
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-03-27 18:12:43 -0300

    Adjust Indentation

--- 4_2/exercise_4.js
+++ 4_2/exercise_4.js
@@ -1,6 +1,6 @@
 let n = 5;
 
-for (let i = 1; i <= n; i+=2) {
+for (let i = 1; i <= n; i += 2) {
     let space = " ".repeat((n - i) / 2)
     let line = "*".repeat(i)
     console.log(space + line + space)

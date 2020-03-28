84df00f0f42c791df9f68012f0f31901fd328ae3
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-03-28 11:18:09 -0300

    Little Fix

--- 4_2/exercise_5.js
+++ 4_2/exercise_5.js
@@ -5,10 +5,10 @@ let base = "*".repeat(n);
 // Top of triangle
 console.log(spaceOut + "*");
 // Between top and base
-for (let i = 3; i <= n - 2 ; i+=2) {
+for (let i = 3; i <= n - 2; i += 2) {
     spaceOut = " ".repeat((n - i) / 2);
     let spaceIn = " ".repeat(n - spaceOut.length * 2 - 2);
     console.log(spaceOut + "*" + spaceIn + "*" + spaceOut);
 }
-// Base of traingle
+// Base of triangle
 console.log(base);

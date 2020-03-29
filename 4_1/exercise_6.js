9acc606c933273f5ecbdf2a184ad8a3776c29a7c
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-03-28 21:28:01 -0300

    Testing

--- 4_1/exercise_6.js
+++ 4_1/exercise_6.js
@@ -3,23 +3,23 @@ let chessPieceLowerCase = chessPiece.toLowerCase();
 
 switch (chessPieceLowerCase) {
     case "king":
-    console.log("One square in any direction")    
+        console.log("One square in any direction")
         break;
     case "queen":
-    console.log("Everywhere")
+        console.log("Everywhere")
         break;
     case "rook":
-    console.log("Straight")
+        console.log("Straight")
         break;
     case "bishop":
-    console.log("Diagonal")
+        console.log("Diagonal")
         break;
     case "knight":
-    console.log("L move")
+        console.log("L move")
         break;
     case "pawn":
-    console.log("Forward one square")
+        console.log("Forward one square")
     default:
-    console.log("ERRO: This is not a chess piece")    
+        console.log("ERRO: This is not a chess piece")
         break;
-} 
+}

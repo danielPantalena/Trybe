a0339ced6faa3bcbcc55300fa4556ac129ce77f4
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-03-30 15:48:35 -0300

    Add Functions

--- 4_1/exercise_2.js
+++ 4_1/exercise_2.js
@@ -1,10 +1,25 @@
-let a = 10;
-let b = 20;
-let result = null;
+// Exercício 2 4.1
 
-if (a > b) {
-    console.log("O maior número é: " + a)
+// let a = 10;
+// let b = 20;
+// let result = null;
+
+// if (a > b) {
+//     console.log("O maior número é: " + a)
+// }
+// else {
+//     console.log("O maior número é: " + b)
+// }
+
+// Refazendo o exercício com Função
+function theHighestIs (a, b) {
+    let result = null;
+    if (a > b) {
+        result = a;
+    }
+    else {
+        result = b;
+    }
+    return result;
 }
-else {
-    console.log("O maior número é: " + b)
-}
+console.log(theHighestIs(100, 20))
--- 4_1/exercise_3.js
+++ 4_1/exercise_3.js
@@ -1,13 +1,29 @@
-let a = 10;
-let b = 20;
-let c = 30;
+// Exercício 3
 
-if (a > b && a > c) {
-    console.log("O maior número é: " + a)
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// if (a > b && a > c) {
+//     console.log("O maior número é: " + a)
+// }
+// else if (b > c) {
+//     console.log("O maior número é: " + b)
+// }
+// else {
+//     console.log("O maior número é: " + c)
+// }
+
+// Refazendo com Função
+function isTheHighest (a, b, c) {
+    let result = null;
+    if (a > b && a > c) {
+        result = a;
+    } else if (b > c) {
+        result = b;
+    } else {
+        result = c;
+    }
+    return result;
 }
-else if (b > c) {
-    console.log("O maior número é: " + b)
-}
-else {
-    console.log("O maior número é: " + c)
-}
+console.log(isTheHighest(10, 20, 30));

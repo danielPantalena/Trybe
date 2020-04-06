3cbfda239bbb30424a8aad0a02d9c8fc8faedb79
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-06 09:50:30 -0300

    improving

--- 4_7/app.js
+++ 4_7/app.js
@@ -1,44 +1,64 @@
 window.onload = function () {
 
-    if (localStorage) {
-        document.body.style.backgroundColor = localStorage.backgroundColor;
-        document.body.style.color = localStorage.color;
-        document.body.style.fontSize = localStorage.fontSize + "px";
-        document.body.style.lineHeight = localStorage.lineHeight + "px";
-        document.body.style.fontFamily = localStorage.fontFamily;
-
-        let button = document.getElementById("button")
-        button.style.backgroundColor = localStorage.color;
-        button.style.color = localStorage.backgroundColor;
-    }
-
+    let settingsButton = document.getElementById("settingsButton");
+    let settings = document.getElementById("settings");
     let backGround = document.getElementById("background-input");
     let color = document.getElementById("color");
     let fontSize = document.getElementById("font-size");
     let lineHeight = document.getElementById("line-height");
     let fontFamily = document.getElementById("font-family");
 
-    document.getElementById("button").addEventListener("click", function () {
-        if (backGround.value != '') {
-            console.log(backGround.value);
-            document.body.style.backgroundColor = backGround.value;
-            localStorage.backgroundColor = backGround.value;
-        }
-        if (color.value != '') {
-            document.body.style.color = color.value;
-            localStorage.color = color.value;
-        }
-        if (fontSize.value != '') {
-            document.body.style.fontSize = fontSize.value + "px";
-            localStorage.fontSize = fontSize.value;
-        }
-        if (lineHeight.value != '') {
-            document.body.style.lineHeight = lineHeight.value + "px"
-            localStorage.lineHeight = lineHeight.value;
-        }
-        if (fontFamily.value != '') {
-            document.body.style.fontFamily = fontFamily.value;
-            localStorage.fontFamily = fontFamily.value;
-        }
+    if (typeof (Storage) != "undefined") {
+        document.body.style.backgroundColor = localStorage.backgroundColor;
+        backGround.value = localStorage.backgroundColor;
+
+        document.body.style.color = localStorage.color;
+        color.value = localStorage.color;
+
+        document.body.style.fontSize = localStorage.fontSize + "px";
+        fontSize.value = localStorage.fontSize;
+
+        document.body.style.lineHeight = localStorage.lineHeight + "px";
+        lineHeight.value = localStorage.lineHeight;
+
+        document.body.style.fontFamily = localStorage.fontFamily;
+
+        let button = document.getElementById("settings")
+        button.style.backgroundColor = localStorage.color;
+        button.style.color = localStorage.backgroundColor;
+    }
+
+    settingsButton.addEventListener("click", function () {
+        if (settings.style.display == "none")
+            settings.style.display = "block";
+        else
+            settings.style.display = "none";
+    })
+
+    backGround.addEventListener("change", function () {
+        document.body.style.backgroundColor = backGround.value;
+        settings.style.color = backGround.value;
+    })
+
+    color.addEventListener("change", function() {
+        document.body.style.color = color.value;
+        settings.style.backgroundColor = color.value;
+    })
+
+    fontSize.addEventListener("change", function() {
+        document.getElementById("article").style.fontSize = fontSize.value + "px";
+    })
+
+    lineHeight.addEventListener("change", function() {
+        document.getElementById("article").style.lineHeight = lineHeight.value + "px";
+    })
+
+
+    document.getElementById("storeButton").addEventListener("click", function () {
+        localStorage.backgroundColor = backGround.value;
+        localStorage.color = color.value;
+        localStorage.fontSize = fontSize.value;
+        localStorage.lineHeight = lineHeight.value;
+        localStorage.fontFamily = fontFamily.value;
     })
 }

252f31d44ab98de36cd5e9319b53cfe4926ed7b5
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-14 16:36:12 -0300

    regular expression

--- block6/6_1/script.js
+++ block6/6_1/script.js
@@ -1,11 +1,24 @@
-const emailRegEx = /@/;
+const regExpEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm;
 
-const email = document.getElementById('email');
-
-const emailValidation = emailRegEx.test(email.value);
-
-email.addEventListener('input', () => {
-    console.log(emailValidation);
-})
+// trying regExpEmail
+const email1 = 'daniel@gmail.com';
+const email2 = 'email  @  .x';
+console.log(regExpEmail.test(email1));
+console.log(regExpEmail.test(email1));
 
 
+// validating email
+const fieldSet = document.getElementById('fieldset');
+const emailInput = document.getElementById('email');
+const brTagAfterEmail = document.getElementById('after-email');
+const validation = document.createElement('span');
+fieldSet.insertBefore(validation, brTagAfterEmail);
+
+emailInput.addEventListener('input', () => {
+    if (regExpEmail.test(emailInput.value)) {
+        validation.innerHTML = '';
+        validation.innerHTML = 'It\'s a valid email :)';
+    } else {
+        validation.innerHTML = 'It\'s not valid email :/';
+    }
+});

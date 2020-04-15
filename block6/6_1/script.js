42e05e55e8747d20927cfbecbaf3edd84b19aeae
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-15 18:11:30 -0300

    add .gitignore

--- block6/6_1/script.js
+++ block6/6_1/script.js
@@ -1,39 +1,28 @@
+// Creating options
 
-// Regular expression for email
-const regExpEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm;
+// Validating email
+const regExpEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/gim;
 
-// // trying regExpEmail
-// const email1 = 'daniel@gmail.com';
-// const email2 = 'email  @  .x';
-// console.log(regExpEmail.test(email1));
-// console.log(regExpEmail.test(email1));
-
-// validating email
 const fieldSet = document.getElementById('fieldset');
 const emailInput = document.getElementById('email');
 const brTagAfterEmail = document.getElementById('after-email');
 const validationEmailMessage = document.createElement('span');
 
 function validateEmail() {
-    emailInput.addEventListener('input', () => {
-        if (regExpEmail.test(emailInput.value))
-            validationEmailMessage.innerHTML = 'It\'s a valid email :)';
-        else if (emailInput.value === '')
-            validationEmailMessage.innerHTML = null;
-        else
-            validationEmailMessage.innerHTML = 'It\'s <strong>NOT</strong> a valid email :/';
-        fieldSet.insertBefore(validationEmailMessage, brTagAfterEmail);
-    });
+  emailInput.addEventListener('input', () => {
+    const validMessage = "It's a valid email :)";
+    const notValidMessage = "It's <strong>NOT</strong> a valid email :/";
+    if (regExpEmail.test(emailInput.value)) {
+      validationEmailMessage.innerHTML = validMessage;
+    } else if (emailInput.value === '') validationEmailMessage.innerHTML = null;
+    else {
+      validationEmailMessage.innerHTML = notValidMessage;
+    }
+    fieldSet.insertBefore(validationEmailMessage, brTagAfterEmail);
+  });
 }
 // validating data
 const regExpDate = /^\d{1,2}\/\d{1,2}\/\d{4}/;
-// // trying regExpDate
-// const date1 = '1/1/1111';
-// const date2 = '26/12/1989';
-// const date3 = '22/2';
-// console.log(regExpDate.test(date1));
-// console.log(regExpDate.test(date2));
-// console.log(regExpDate.test(date3));
 
 const fieldSet2 = document.getElementById('fieldset2');
 const dataInput = document.getElementById('data');
@@ -41,19 +30,20 @@ const brAfterData = document.getElementById('after-data');
 const validationDateMessage = document.createElement('span');
 
 function validateDate() {
-    dataInput.addEventListener('input', () => {
-        console.log(regExpDate.test(dataInput.value));
-        if (regExpDate.test(dataInput.value))
-            validationDateMessage.innerHTML = `It's a valid date :)`;
-        else if (dataInput.value === '')
-            validationDateMessage.innerHTML = null;
-        else 
-            validationDateMessage.innerHTML = `It's <strong>NOT</strong> a valid date :/`;
-    });
-    fieldSet2.insertBefore(validationDateMessage, brAfterData);
+  const validMessage = "It's a valid date :)";
+  const notValidMessage = "It's <strong>NOT</strong> a valid date :/";
+  dataInput.addEventListener('input', () => {
+    if (regExpDate.test(dataInput.value)) {
+      validationDateMessage.innerHTML = validMessage;
+    } else if (dataInput.value === '') validationDateMessage.innerHTML = null;
+    else {
+      validationDateMessage.innerHTML = notValidMessage;
+    }
+  });
+  fieldSet2.insertBefore(validationDateMessage, brAfterData);
 }
 
 window.onload = () => {
-    validateEmail();
-    validateDate();
+  validateEmail();
+  validateDate();
 };

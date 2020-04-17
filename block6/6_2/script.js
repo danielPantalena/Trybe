3561ee4b9b89340e748b5c7a1b8be2978b187bfe
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-17 20:18:29 -0300

    Initial commit

---
+++ block6/6_2/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('select');
+    let options = document.querySelectorAll('option');
+    let instances = M.FormSelect.init(elems, options);
+  });

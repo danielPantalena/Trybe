05159b81d862e7246a99a4fda790c53cba1d1124
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-18 11:22:52 -0300

    css done

--- block6/6_2/script.js
+++ block6/6_2/script.js
@@ -1,5 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('select');
-    let options = document.querySelectorAll('option');
-    let instances = M.FormSelect.init(elems, options);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  let elems = document.querySelectorAll('select');
+  let options = document.querySelectorAll('option');
+  let instances = M.FormSelect.init(elems, options);
+});
+
+window.onload = () => {
+
+};

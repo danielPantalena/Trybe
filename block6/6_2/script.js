340aa654c14bf923b104e9edcea1a2806eb19301
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-18 17:05:20 -0300

    add jQuery

--- block6/6_2/script.js
+++ block6/6_2/script.js
@@ -1,9 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let elems = document.querySelectorAll('select');
-  let options = document.querySelectorAll('option');
-  let instances = M.FormSelect.init(elems, options);
+$(document).ready(function(){
+  $('select').formSelect();
 });
-
-window.onload = () => {
-
-};

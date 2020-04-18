d7cbbe5e458107c99d4c3abf3dbdb71ec7085a2f
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-18 19:24:30 -0300

    add buttons

--- block6/6_2/script.js
+++ block6/6_2/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
+// initialize the select element
+$(document).ready(function () {
   $('select').formSelect();
 });
+
+// submit form
+function submitForm(event) {
+  event.preventDefault();
+  const infos = event.target.elements;
+  const resultsDiv = document.createElement('div');
+  resultsDiv.classList.add('container');
+  for (let i = 0; i < infos.length; i += 1) {
+    if (infos[i].id !== '') {
+      let resultChild = document.createElement('p');
+      resultChild.innerHTML = `${infos[i].name}: ${infos[i].value} <br>`;
+      resultsDiv.appendChild(resultChild);
+    }
+  }
+  document.body.appendChild(resultsDiv);
+}
+
+window.onload = () => {
+  document.getElementById('form').addEventListener('submit', submitForm);
+};

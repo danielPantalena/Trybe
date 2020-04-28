fa079f42032e594aaee1269e2ed72d4433d85689
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-28 19:41:41 -0300

    done

--- 7-block/3/script.js
+++ 7-block/3/script.js
@@ -1,6 +1,43 @@
 const deck = document.getElementById('deck');
+const cardsContainer = document.querySelector('div.container');
+const cards = [
+  'cinco-de-ouros',
+  'nove-de-espadas',
+  'quatro-de-paus',
+  'seis-de-copas',
+  'sete-de-espadas',
+  'sete-de-paus',
+  'tres-de-ouros',
+];
+const animations = ['rotate', 'grow', 'flip', 'front-flip'];
 
+function randomPick(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+function pickCard() {
+  deck.addEventListener('click', () => {
+    const card = document.createElement('img');
+    card.src = `imgs/cartas/${randomPick(cards)}.png`;
+    cardsContainer.appendChild(card);
+  });
+}
+function resetAnimation(element) {
+  element.className = '';
+}
+function animateCards() {
+  cardsContainer.addEventListener('mouseover', (e) => {
+    if (e.target == cardsContainer) return null;
+    else if (e.target.className != '') return null;
+    e.target.classList.add(randomPick(animations));
+    console.log(e.target);
+    setTimeout(() => {
+      resetAnimation(e.target);
+      console.log(e.target);
+    }, 1500);
+  });
+}
 
 window.onload = function () {
-
-}
+  this.pickCard();
+  this.animateCards();
+};

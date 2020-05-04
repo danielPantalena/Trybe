b4647a46cad8c5a1b72a9047f3716b67ae7b5882
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-04 15:56:51 -0300

    remove file

--- 8-block/3/pre-aula.js
+++
@@ -1,9 +0,0 @@
-const myObject = {};
-
-function introduceToObject(object, key, value) {
-  const newKey = key;
-  object[newKey] = value;
-  console.log(object);
-}
-
-introduceToObject(myObject, 'name', 'daniel');

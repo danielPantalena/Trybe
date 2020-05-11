114c336ebe90da04701864866c7abca5da2bfb1a
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-11 15:21:09 -0300

    to oneline

--- 9-block/2/pre.js
+++ 9-block/2/pre.js
@@ -1,10 +1,6 @@
 const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
 const prices = [2.99, 3.99, 1.5, 2];
 
-const arraysToObject = (arr1, arr2) => {
-  result = {};
-  arr1.map((product, index) => result[product] = arr2[index]);
-  return result;
-}
+const arraysToObject = (arr1, arr2) => arr1.map((p, i) => ({[p] : arr2[i]}));
 
 console.log(arraysToObject(products, prices));

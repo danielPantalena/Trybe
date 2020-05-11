194e2df7e4211e9d5cbd08794874f00b02346c91
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-11 16:02:26 -0300

    exercise1

--- 9-block/2/pre.js
+++
@@ -1,6 +0,0 @@
-const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
-const prices = [2.99, 3.99, 1.5, 2];
-
-const arraysToObject = (arr1, arr2) => arr1.map((p, i) => ({[p] : arr2[i]}));
-
-console.log(arraysToObject(products, prices));

a0ddc42c7cd8e68f05a71499fc0f4fb5e717bfc0
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-12 18:12:17 -0300

    exercise6

---
+++ 9-block/3/exercise5.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+
+const myList = [1, 2, 3]
+const myListObj = {a: 1, b: 2, c: 3};
+
+const {a, b, c} = myListObj;
+console.log(`${a} ${b} ${c}`);
+
+// escreva swap abaixo
+// const swap = (arr) => {};
+
+// const swappedList = swap(myList);
+
+// assert.equal(swappedList[0], 3)
+// assert.equal(swappedList[1], 2)
+// assert.equal(swappedList[2], 1)
+    

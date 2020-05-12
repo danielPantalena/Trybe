b56ebba87a650b00e94a681b36c7ff41d2a9e343
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-12 19:08:23 -0300

    Done

--- 9-block/3/exercise8.js
+++ 9-block/3/exercise8.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 // escreva greet abaixo
-const greet = (name, greetPhrase = 'Hi') => `${greetPhrase} ${name}`;
+const greet = (name, greetString = 'Hi') => `${greetString} ${name}`;
 
 assert.equal(greet('John'), 'Hi John');
 assert.equal(greet('John', 'Good morning'), 'Good morning John');

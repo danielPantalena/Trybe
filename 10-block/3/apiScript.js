0928a8eb60dc272444a74e793ce62c15dc0e0e98
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-20 20:15:33 -0300

    exercise3

--- 10-block/3/apiScript.js
+++ 10-block/3/apiScript.js
@@ -11,10 +11,44 @@ const fetchJoke = () => {
     .then(data => addJokeToHtml(data.joke));
 };
 
-const addJokeToHtml = (jokeMessage) => {
+const addJokeToHtml = jokeMessage => {
   const jokeHTMLelement = document.createElement('p');
   jokeHTMLelement.innerHTML = jokeMessage;
   document.getElementById('jokeContainer').appendChild(jokeHTMLelement);
-}
+};
+
+const randomNumber = num => Math.random() * (num - 1) + 1;
+
+const createArray = num => {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(Math.pow(randomNumber(50), 2));
+  }
+  return arr;
+};
+
+const arrayNumbers = createArray(10);
+
+const arrSumFun = arr => arr.reduce((total, num) => total + num);
+
+const arrSum = arrSumFun(arrayNumbers);
+
+
+const isArrSumLowerThan = () => arrSum < 8000;
+
+console.log('isLT 8000?', isArrSumLowerThan());
+
+const p = new Promise((resolve, reject) => {
+  if (isArrSumLowerThan()) resolve('resolved called');
+  else reject('rejected');
+});
+
+p.then(message => {
+  console.log(message, 'then called');
+  console.log([arrSum/2, arrSum/3, arrSum/5, arrSum/10]);
+}).catch(err => {
+  console.log(err, 'catch called');
+  console.log('É mais de 8000');
+});
 
 window.onload = () => fetchJoke();

a79b0b7c3cbdd53fcb91a0cb5f3904bf95717f86
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-27 15:09:42 -0300

    Done!

--- 11-block/2/exercise5.test.js
+++ 11-block/2/exercise5.test.js
@@ -2,13 +2,13 @@ let functions = require('./exercises4n5');
 
 describe('Exercise5', () => {
   test('upperCase -> lowerCase', () => {
-    const mockUpperCase = jest
+    jest
       .spyOn(functions, 'upperCase')
       .mockImplementation(str => str.toLowerCase());
     expect(functions.upperCase('AaA')).toBe('aaa');
-
+  });
+  test('upperCase restored', () => {
     functions.upperCase.mockRestore();
-
     expect(functions.upperCase('aAa')).toBe('AAA');
   });
 });
--- 11-block/2/exercise6.js
+++ 11-block/2/exercise6.js
@@ -1,14 +1,10 @@
 const fetch = require('node-fetch');
-const API_URL = 'https://dog.ceo/api/breeds/image/random'
+const API_URL = 'https://dog.ceo/api/breeds/image/random';
 
 const fetchDog = async () => {
   const resp = await fetch(API_URL);
-  const data = await resp.json();
+  const data = resp.json();
   return data;
 };
 
-fetchDog()
-  .then(resp => resp)
-  .then(data => console.log(data));
-
-module.exports = fetchDog;
+module.exports = { fetchDog };

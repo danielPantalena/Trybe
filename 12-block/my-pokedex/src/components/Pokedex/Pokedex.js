1b4179fd31d5ab34fca92f79bfb60e5d25e7c065
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-05-30 21:41:13 -0300

    Done :)

--- 12-block/my-pokedex/src/components/Pokedex/Pokedex.js
+++ 12-block/my-pokedex/src/components/Pokedex/Pokedex.js
@@ -1,1 +1,16 @@
 import React from 'react';
+import './Pokedex.css'
+import Pokemon from '../Pokemon/Pokemon';
+
+class Pokedex extends React.Component {
+  render() {
+    const {pokemons} = this.props
+    return (
+      <div className='pokedex'>
+        {pokemons.map((poke) => <Pokemon key={poke.id} pokemon={poke}/>)}
+      </div>
+    )
+  }
+}
+
+export default Pokedex;

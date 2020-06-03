9ab4d37e42923b306858ab067ea325f67787aa1a
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-02 22:21:38 -0300

    Done first part

--- 12-block/my-pokedex/src/App.js
+++ 12-block/my-pokedex/src/App.js
@@ -3,32 +3,62 @@ import './App.css';
 import pokemonsObjs from './data';
 import Pokedex from './components/Pokedex/Pokedex';
 import AddPokeButton from './components/AddPokeButton/AddPokeButton';
+import TypeButton from './components/TypeButton/TypeButton';
 
-const pokemonsArray = [];
+let indexOfPoke = 0;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemons: [],
+      pokemon: [pokemonsObjs[indexOfPoke]],
+      pokemonsArray: pokemonsObjs,
     };
   }
 
-  addPoke = () => {
-    const numberOfPokes = this.state.pokemons.length;
-    if (numberOfPokes < pokemonsObjs.length) {
-      pokemonsArray.push(pokemonsObjs[numberOfPokes]);
-      this.setState({
-        pokemons: pokemonsArray,
+  nextPoke = () => {
+    if (indexOfPoke === this.state.pokemonsArray.length - 1) indexOfPoke = -1;
+    indexOfPoke += 1;
+    console.log(this.state)
+    console.log(indexOfPoke)
+    this.setState({
+      pokemon: [this.state.pokemonsArray[indexOfPoke]],
+    });
+  };
+
+  filterType = (typeButton) => {
+    indexOfPoke = -1;
+    if (typeButton === 'All') {
+      console.log(this.state)
+    console.log(indexOfPoke)
+      return this.setState({
+        pokemonsArray: pokemonsObjs,
       });
     }
+    const filteredArray = pokemonsObjs.filter(
+      ({ type }) => type === typeButton,
+    );
+    console.log(this.state)
+    console.log(indexOfPoke)
+    this.setState({
+      pokemonsArray: filteredArray,
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <AddPokeButton handleClick={this.addPoke} />
-        <Pokedex pokemons={this.state.pokemons} />
+        <Pokedex pokemons={this.state.pokemon} />
+        <TypeButton handleClick={this.filterType} type="All" />
+        <TypeButton handleClick={this.filterType} type="Bug" />
+        <TypeButton handleClick={this.filterType} type="Dragon" />
+        <TypeButton handleClick={this.filterType} type="Electric" />
+        <TypeButton handleClick={this.filterType} type="Fire" />
+        <TypeButton handleClick={this.filterType} type="Normal" />
+        <TypeButton handleClick={this.filterType} type="Poison" />
+        <TypeButton handleClick={this.filterType} type="Psychic" />
+        <br />
+        <AddPokeButton handleClick={this.nextPoke} />
       </div>
     );
   }
--- 12-block/my-pokedex/src/components/AddPokeButton/AddPokeButton.js
+++ 12-block/my-pokedex/src/components/AddPokeButton/AddPokeButton.js
@@ -3,9 +3,7 @@ import React from 'react';
 class AddPokeButton extends React.Component {
   render() {
     // console.log('this.props Button', this.props)
-    return (
-      <button onClick={this.props.handleClick}>Add Poke</button>
-    );
+    return <button onClick={this.props.handleClick}>Next Poke</button>;
   }
 }
 

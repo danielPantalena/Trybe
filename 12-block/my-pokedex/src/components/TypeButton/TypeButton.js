158a70ae38faffc0640f096dfdcac0679993081e
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-03 07:24:40 -0300

    add TypeButton component

---
+++ 12-block/my-pokedex/src/components/TypeButton/TypeButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TypeButton extends React.Component {
+  render () {
+    const {type, handleClick } = this.props;
+    return (
+    <button onClick={() => handleClick(type)}>{type}</button>
+    )
+  }
+}
+
+export default TypeButton;

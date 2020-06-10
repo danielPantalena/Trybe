3d165bdd44eed330c42624ba9bdaf4d439126371
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-10 18:56:25 -0300

    Done parte 1

--- 13-block/3/router-react/src/App.tsx
+++ 13-block/3/router-react/src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './component/About';
+import Home from './component/Home';
+import Users from './component/Users';
+import User from './component/User';
+import StrictAcess from './component/StrictAcess';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={About} path="/about" />
+          <Route path="/users/:id" component={User} />
+          <Route
+            path="/strict-acess"
+            render={(props) => <StrictAcess {...props} username={'João'} password={'1234'} />}
+          />
+          <Route
+            render={(props) => <Users {...props} greetingMessage={'Good Morning'} />}
+            path="/users"
+          />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
